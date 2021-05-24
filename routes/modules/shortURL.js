e497dfecd1883efd45f1cd33b4f15f4e98accb0e
Rita <hsen.yeh123@gmail.com>
2021-05-24 21:04:47 +0800

    fix: add function in shortURL.js

--- routes/modules/shortURL.js
+++ routes/modules/shortURL.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const router = express.Router()
-const shortid = require('shortid')
 const Link = require('../../models/link')
 
 
+function getRandomString(length) {
+  const randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  let result = ''
+  for (let i = 0; i < length; i++) {
+    result += randomChars.charAt(Math.floor(Math.random() * randomChars.length))
+  }
+  return result
+}
+
+
+
 router.post('/', async (req, res) => {
+  //const uid = new ShortUniqueId()
   await Link.create(
     {
       full: req.body.fullURL,
-      short: shortid.generate(),
+      short: getRandomString(5)
+
     })
     .then(() => { res.redirect('/') })
     .catch(error => console.log('error'))
